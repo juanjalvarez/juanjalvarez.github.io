254565a52de3c85463bae15a22f6467f2f1791ff
Juan J. Alvarez <jalvarez5241997@gmail.com>
2018-01-15 18:00:40 -0400

    switched to hashrouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { HashRouter as Router, Route } from 'react-router-dom'
 
 import NavBar from './NavBar'
 import Main from './Main'
